2b52c477143854b3373d7ceb5d75eef80bc79d64
pramoddeqode <pryadav@deqode.com>
2022-09-08 18:26:36 +0530

    login  & home created

--- Firstproject/src/screens/HomeScreen.js
+++ Firstproject/src/screens/HomeScreen.js
@@ -1,14 +1,86 @@
+import { BaseRouter } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView,TouchableOpacity,Text,View} from 'react-native';
+import { SafeAreaView,TouchableOpacity,Text,View, Image,FlatList,StyleSheet} from 'react-native';
 
 const HomeScreen = () => {
+    const Data = [
+        {
+          id: '1',
+          title: 'John Doy',
+          img:require('../assets/images/man1.png')
+        },
+        {
+            id: '2',
+            title: 'Roman Doy',
+            img:require('../assets/images/man2.jpeg')
+          },
+        
+      ];
   return (
-      <SafeAreaView>
-        <TouchableOpacity >
-          <Text>Home Screen</Text>
-        </TouchableOpacity>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <FlatList
+             data={Data}
+             renderItem={({item})=>(
+                <View>
+            <View style={styles.row}> 
+                <TouchableOpacity style={styles.imgback}>
+                  <Image style={styles.img} source={item.img} />
+               </TouchableOpacity>
+               <Text style={styles.title}>{item.title}</Text>
+             </View>
+             <View style={styles.line}/>
+             </View>
+        )}
+        />
+        </View>
+       
+    
       </SafeAreaView>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    img:{
+      resizeMode:'contain' ,
+      height:60,
+      width:60,
+      borderRadius:25
+      
+    },
+    imgback:{
+       backgroundColor:"white",
+       borderRadius:35, 
+       height:65,
+       width:65,
+       alignItems:'center',
+       justifyContent:'center',
+       borderColor:'gray',
+       borderWidth:1
+    },
+    row:{
+        flexDirection:"row",
+        alignItems:'center',
+       margin:10
+    },
+    title:{
+        color:'black',
+        fontSize:22,
+        fontWeight:'600',
+        marginLeft:15
+       
+     },
+     line:{
+        borderWidth:0.3,
+        borderBottomColor:'gray',
+        marginVertical:8
+        
+     }
+}
+)
+   
--- Firstproject/src/screens/LoginScreen.js
+++ Firstproject/src/screens/LoginScreen.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet,Text, View ,TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet,Text, View ,TouchableOpacity,TextInput} from 'react-native';
 
 
 const LoginScreen = (props) => {
+    const [email ,setemail] = useState('');
+    const [password , setpassword] = useState('');
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.Heading}>Enter Email</Text>
-
+      <View style={styles.main}> 
+        <Text style={styles.Heading}>Email</Text>
+        <TextInput
+        style={styles.input}
+        onChangeText={(value)=> setemail(value)}
+        value={email}
+        placeholder="Enter Email"
+        keyboardType="email-address"
+      />
       </View>
+      <View style={styles.main}> 
+        <Text style={styles.Heading}>Password</Text>
+        <TextInput
+           style={styles.input}
+           onChangeText={(value)=> setpassword(value)}
+           value={password}
+           placeholder="Enter Password"
+           keyboardType="phone-pad"
+      />
+      </View> 
+       <TouchableOpacity style={styles.login} onPress={()=> props.navigation.navigate('HomeScreen')}>
+        <Text style={styles.logintxt}>Login</Text>
+       </TouchableOpacity>
   </SafeAreaView>
   );
 };
@@ -22,7 +43,35 @@ container:{
 Heading:{
    color:'black',
    fontSize:16,
-   fontWeight:'600' 
-}
+   fontWeight:'600',
+   marginLeft:15
+},
+input:{
+    height:40,
+    borderWidth:1,
+    borderRadius:8,
+    paddingLeft:10,
+    borderColor:'gray',
+    marginVertical:5,
+    marginHorizontal:15
 
+    },
+    main:{
+        marginTop:20
+    },
+    login:{
+         backgroundColor:'green',
+         borderRadius:15,
+         padding:10,
+         width:'50%',
+         alignSelf:'center',
+         marginTop:50
+    },
+    logintxt:{
+        color:'white',
+        fontSize:18,
+        fontWeight:'600',
+        textAlign:'center'
+        
+    }
 })
