7423d1d50b408d702bc12bf5ff7d6043647aee47
pramoddeqode <pryadav@deqode.com>
2022-09-29 12:32:10 +0530

    thunk

--- DemoThunk/App.js
+++ DemoThunk/App.js
@@ -1,7 +1,9 @@
 import {Provider} from 'react-redux';
 import configureStore from './src/reduxThunk/store/configureStore';
 import Home from './src/screens/Home';
-const store = configureStore()
+
+const store = configureStore();
+
 export default function App() {
   return (
     <Provider store={store}>
